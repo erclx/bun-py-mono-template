8284f2b02f793dfe55ff685deedebe189a53c8fe
Eric Le <eriiicle@outlook.com>
2025-12-07 17:35:12 +0100

    feat(backend): implement proper settings configuration and cors middleware

--- apps/backend/src/app/main.py
+++ apps/backend/src/app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title='UV Python Template')
+from .config import settings
+
+app = FastAPI(title='Bun Py Backend API')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-    ],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
